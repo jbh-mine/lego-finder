48a024ece8a230c2fe575a519743f3d9931ff639
jbh-mine <sinhwapen@gmail.com>
2026-04-06 17:31:41 +0900

    Fix searchDict.js: rename searchQuery to translateSearchQuery and export it

--- src/utils/searchDict.js
+++ src/utils/searchDict.js
@@ -26,17 +26,17 @@ var SET_NUM_MAP = {
   '\ud574\uc801\uc120': '10365',
   '\uc7ad\uc2a4\ud328\ub85c\uc6b0': '10365',
   '\uc7ad \uc2a4\ud328\ub85c\uc6b0': '10365',
-  '\ucce90\ub9ac\ube44\uc548\uc758\ud574\uc801': '10365',
-  '\ucce90\ub9ac\ube44\uc548 \ud574\uc801': '10365',
+  '\ucce8\ub9ac\ube44\uc548\uc758\ud574\uc801': '10365',
+  '\ucce8\ub9ac\ube44\uc548 \ud574\uc801': '10365',
   '\ud0c0\uc774\ud0c0\ub2c9': '10294',
   '\ucf5c\ub85c\uc138\uc6c0': '10276',
   '\uc5d0\ud384\ud0d1': '10307',
-  '\ub098\ubbac\uc9d1': '21318',
+  '\ub098\ubb34\uc9d1': '21318',
   '\ud2b8\ub9ac\ud558\uc6b0\uc2a4': '21318',
   '\uc911\uc138\ub300\uc7a5\uac04': '21325',
   '\uc911\uc138 \ub300\uc7a5\uac04': '21325',
   '\uadf8\ub79c\ub4dc\ud53c\uc544\ub178': '21323',
-  '\uc9c0\uad6c\ubbfc': '21332',
+  '\uc9c0\uad6c\ubcf8': '21332',
   '\ud0c0\uc790\uae30': '21327',
   '\ucf58\ucf54\ub4dc': '10318',
   '\ubc00\ub808\ub2c8\uc5c4\ud31c\ucf58': '75192',
@@ -108,6 +108,8 @@ var PHRASE_MAP = [
   ['\ud574\ub9ac\ud3ec\ud130', 'Harry Potter'],
   ['\ud574\ub9ac \ud3ec\ud130', 'Harry Potter'],
   ['\ud638\uadf8\uc640\ud2b8 \uc775\uc2a4\ud504\ub808\uc2a4', 'Hogwarts Express'],
+  ['\ud638\uadf8\uc640\ud2b8 \uc131', 'Hogwarts Castle'],
+  ['\ud638\uadf8\uc640\ud2b8\uc131', 'Hogwarts Castle'],
   ['\ud638\uadf8\uc640\ud2b8\ub9ac\ub9c1', 'Hogsmeade'],
   ['\ud638\uadf8\ub9c8\ub9ac', 'Hogsmeade'],
   ['\ub530\ub978\ub0b4\ub0e8', 'Diagon Alley'],
@@ -117,13 +119,12 @@ var PHRASE_MAP = [
   // ===== Pirates =====
   ['\ube44\ub098\uce6d', 'Pirates'],
   ['\ube44\ub098\uce6d\ub0b4\uc678', 'Pirates'],
-  ['\ubbe0\ub728\ubc18\ub0ae', 'Black Pearl'],
   ['\ube14\ub799 \ud384', 'Black Pearl'],
-  ['\ucce90\ub9ac\ube44\uc548\uc758 \ud574\uc801', 'Pirates of the Caribbean'],
-  ['\ucce90\ub9ac\ube44\uc548 \ud574\uc801', 'Pirates of the Caribbean'],
-  ['\uc7ad\uc2a4\ud328\ub85c\uc6b0', "Jack Sparrow"],
-  ['\uc7ad \uc2a4\ud328\ub85c\uc6b0', "Jack Sparrow"],
-  ['\uccea\ud134 \uc7ad \uc2a4\ud328\ub85c\uc6b0', "Captain Jack Sparrow"],
+  ['\ucce8\ub9ac\ube44\uc548\uc758 \ud574\uc801', 'Pirates of the Caribbean'],
+  ['\ucce8\ub9ac\ube44\uc548 \ud574\uc801', 'Pirates of the Caribbean'],
+  ['\uc7ad\uc2a4\ud328\ub85c\uc6b0', 'Jack Sparrow'],
+  ['\uc7ad \uc2a4\ud328\ub85c\uc6b0', 'Jack Sparrow'],
+  ['\uccea\ud134 \uc7ad \uc2a4\ud328\ub85c\uc6b0', 'Captain Jack Sparrow'],
 
   // ===== Modular Buildings =====
   ['\uCE74\uD398\uCF54\uB108', 'Cafe Corner'],
@@ -172,142 +173,73 @@ var PHRASE_MAP = [
 
 var WORD_MAP = {
   '\ub2e4\uc774\uc560\ub7a9\uac74': 'Diagon Alley',
-  '\ub098\ubbac': 'Cottage',
-  '\ub514\ub978\ub0b4\ub0e8': 'Diagon Alley',
-  '\ub514\ub978\uac70\ub9ac': 'Diagon Alley',
-  '\uac70\ub300': 'Large',
-  '\ub2e8\uc0c1': 'Platform',
+  '\ub098\ubb34': 'Tree',
   '\ubaa8\ub4c8\ub7ec': 'Modular',
-  '\uce74\uc815': 'Castle',
-  '\uc84c\uac31': 'Castle',
-  '\uc84c\uac41': 'Castle',
-  '\uc2a4\ud0c0\uc654\uc988': 'Star Wars',
+  '\uc2a4\ud0c0\uc6cc\uc988': 'Star Wars',
   '\uc2a4\ud0c0': 'Star',
-  '\uc678\uad6c\ub978': 'Dementor',
-  '\ud0c4\ud0c4\ub85c\ub978': 'Centaur',
-  '\ub9c8\ucd08': 'Broomstick',
-  '\ud37c\ud50c': 'Purple',
-  '\ub04c\ub798\ub9b0': 'Dragon',
-  '\uac70\ub150': 'Mirror',
-  '\ud6d4\ubf88': 'Mirror',
-  '\ube44\ub978': 'Dark',
-  '\ube44\ub978\ub0d0': 'Dark Magic',
-  '\uad70\ubd80': 'Army',
-  '\uba54\ub098\ub9ac': 'Maneuver',
-  '\uba54\ub098': 'Manner',
-  '\ud328\ub85c\ub514': 'Parody',
-  '\ub098\ub975\ub2f5\uac1c': 'Butterfly',
-  '\uc0ac\ub2e8\ub370\uac00\ub098': 'Sneak',
-  '\ud5a5\ub828\ub4ac': 'Fragrant',
-  '\ube44\ucc1c': 'Rain',
-  '\ube0c\ub85c\ucd08\uac8c\uc784': 'Blurry Game',
-  '\ub300\ub2c8': 'Danish',
-  '\ub2ec\uac70\ub9ac\ub294': '달거린',
-  '\ub09c\uc81c': 'Rampage',
-  '\ub09c\ub77c\ub958': 'Rampage',
-  '\ubc30\ub978\ub0d0': 'Bad',
-  '\uac70\uc9f1\ub05b\ub09c\ub9dd': 'Hedgehog',
-  '\ub09c\ubaa8': 'Chaos',
-  '\ub09c\ucc9c\ud55c': 'Reckless',
-  '\uba54\ubaa8\ub9ac': 'Memory',
-  '\uba54\ub85c': 'Melody',
-  '\ub098\ub978\uc1c4': 'Bad Color',
-  '\ub098\uc058\ub2a4\ub078\ub978': 'Miserable',
-  '\ube70\ub978\ub354\ub978\ucc75': 'Pale Red',
-  '\ube70\ub978\ub354\ub978': 'Red',
-  '\ube70\ub978': 'Red',
-  '\ube70\ub978\ube60\aea0': 'Crimson',
-  '\ube70\ub978\uccb4\uc724': 'Burgundy',
-  '\ube70\ub978\ucd00\ub978': 'Chill',
-  '\uba54\ubd80\ub978\ub9c1': 'Membranes',
-  '\uae30\ub77c\ub098\ub294': 'Giraffes',
-  '\uae30\ub77c\ub098': 'Giraffe',
-  '\uae30\ub77c\ud0a4': 'Giraffes',
-  '\ube14\ub979\uc2a4': 'Blocks',
-  '\ube14\ub97a': 'Block',
-  '\ubbb4\ub2ec': 'Heavy',
-  '\ubcf4\ub3c4': 'Report',
-  '\ubcf5\ubb3c\ub098\ub9ac': 'Mysterious',
-  '\ub2de\ub354': 'Finder',
-  '\ub2de\ucc3c': 'Finder',
-  '\ub2de\ucc3c\uc5f0\ub958': 'Finding',
-  '\ub098\ub986': 'Appearing',
-  '\ub098\ub79c': 'Butterfly',
-  '\ub098\ub398': 'Butterfly',
-  '\uafb8\ub294': 'Wrapping',
-  '\uba3c\uc9f1\ub3d5': 'Donkey',
-  '\uba3c\ucc4c': 'Mule',
-  '\ub098\ub978': 'Bad',
-  '\ub098\uc04a': 'Slim',
-  '\ub0a8\ub294': 'Stay',
-  '\ub0a8\uae34': 'Remain',
-  '\ub098\ube20': 'Farewell',
-  '\ubbf8\ub380\ub77c\ub77c': 'Midaralala',
-  '\ub098\ub278\ub77c\ub098': 'Nananana',
-  '\ub098\ub358\ub2fa': 'Nana',
-  '\ubbf8\ub9e8': 'Taste',
-  '\ubcf5\ub2e8\uc701\uc2a4\ub7ec\ub2f5': 'Complex',
-  '\ubbf8\ub798': 'Future',
-  '\ubbf8\ub728\ub099': 'Mirage',
-  '\ubbf8\ub798\ub294': 'Looking ahead',
-  '\ubbf8\ub9c8\uc911': 'Mid-finishing',
-  '\ub098\ub099\ub098\uac15': 'Naing',
-  '\ub098\ub09c': 'Wonderful',
-  '\ub0a8\ub098\ub098\ub098': 'Nanana',
-  '\ub098\ub098\ub0b4\ub77c': 'Nanatra',
-  '\ub098\ub09c\ub77c\ub098': 'Wondrous',
-  '\ub099\ub0b4\ub284': 'Naedan',
-  '\ub0a8\ub2ed': 'Male',
-  '\ub0a8\uae30\ub2f5': 'Southward',
-  '\ub098\ub098\ub098': 'Nana',
-  '\ub098\ub878\ub358': 'Nanada',
-  '\ub098\ubdc0\ub2dd': 'Nabird',
-  '\ub0b4\ub824': 'Descending',
-  '\ub0b4\ub9c8\uc74c': 'Inner mind',
-  '\ub0b4\uc815': 'Inside',
-  '\ub0b4\uc77c\ub798': 'Inside',
-  '\ub0b4\ub760': 'Inside',
-  '\ub0a8\uac70': 'Southerner',
-  '\ub0a8\uac00\ubd84': 'Southern',
-  '\ub09c\ud0c4\ub098\ub098': 'Resistance Nanana',
-  '\ub098\ub3c4': 'Including',
-  '\ub098\ub978\uc81c': 'Bad thing',
-  '\ub098\ub978\ub9e8': 'Bad taste',
-  '\ub098\uba40': 'Far',
-  '\ub098\ub78c': 'Butterfly',
-  '\ub098\ubcc0': 'Butterfly',
-  '\ub098\ub78c\ub098\ub78c': 'Butterfly-butterfly',
+  '\ub9c8\ubc95': 'Magic',
+  '\uc131': 'Castle',
+  '\ud574\uc801': 'Pirate',
+  '\uc6b0\uc8fc': 'Space',
+  '\ub3c4\uc2dc': 'City',
+  '\uae30\ucc28': 'Train',
+  '\ube44\ud589\uae30': 'Airplane',
+  '\ub85c\ubd07': 'Robot',
+  '\ub2cc\uc790\uace0': 'Ninjago',
+  '\ud14c\ud06c\ub2c9': 'Technic',
+  '\ub4c0\ud50c\ub85c': 'Duplo',
+  '\ud504\ub80c\uc988': 'Friends',
+  '\ubc30\ud2b8\ub9e8': 'Batman',
+  '\uc288\ud37c\ud788\uc5b4\ub85c': 'Super Hero',
+  '\ub9c8\uc778\ud06c\ub798\ud504\ud2b8': 'Minecraft',
+  '\ub9c8\uc778 \ud06c\ub798\ud504\ud2b8': 'Minecraft',
+  '\uc544\uc774\ub514\uc5b4': 'Ideas',
+  '\ud06c\ub9ac\uc5d0\uc774\ud130': 'Creator',
+  '\ud06c\ub9ac\uc5d0\uc774\ud130 \uc804\ubb38\uac00': 'Creator Expert',
+  '\uc544\ud0a4\ud14d\ucc98': 'Architecture',
 };
 
-function searchQuery(q) {
+function translateSearchQuery(q) {
   if (!q) return '';
 
-  var result = q;
+  var trimmed = q.trim();
 
-  // Check phrase map first (exact priority)
+  // Check SET_NUM_MAP first (direct nickname to set number)
+  if (SET_NUM_MAP[trimmed]) {
+    return SET_NUM_MAP[trimmed];
+  }
+
+  // Check SET_NUM_MAP with prefix matching
+  var setKeys = Object.keys(SET_NUM_MAP);
+  for (var sk = 0; sk < setKeys.length; sk++) {
+    if (setKeys[sk].indexOf(trimmed) === 0 && trimmed.length >= 2) {
+      return SET_NUM_MAP[setKeys[sk]];
+    }
+  }
+
+  // Check phrase map (exact priority)
   for (var i = 0; i < PHRASE_MAP.length; i++) {
-    if (result.indexOf(PHRASE_MAP[i][0]) !== -1) {
+    if (trimmed.indexOf(PHRASE_MAP[i][0]) !== -1) {
       return PHRASE_MAP[i][1];
     }
   }
 
-  // Check SET_NUM_MAP (for set numbers)
-  if (SET_NUM_MAP[result]) {
-    return SET_NUM_MAP[result];
-  }
-
-  // Word-based search
-  var words = result.split(' ');
+  // Word-based translation
+  var words = trimmed.split(' ');
   var newWords = [];
+  var anyTranslated = false;
   for (var j = 0; j < words.length; j++) {
     var word = words[j];
     if (WORD_MAP[word]) {
       newWords.push(WORD_MAP[word]);
+      anyTranslated = true;
     } else {
       newWords.push(word);
     }
   }
-  result = newWords.join(' ');
+  if (anyTranslated) {
+    return newWords.join(' ');
+  }
 
   // Partial word match
   var bestWord = null;
@@ -315,11 +247,11 @@ function searchQuery(q) {
   var keys = Object.keys(WORD_MAP);
   for (var w = 0; w < keys.length; w++) {
     var wko = keys[w];
-    if (wko.indexOf(q) === 0 && q.length >= 2 && q.length > bestWordLen) {
+    if (wko.indexOf(trimmed) === 0 && trimmed.length >= 2 && wko.length > bestWordLen) {
       bestWord = WORD_MAP[wko];
-      bestWordLen = q.length;
+      bestWordLen = wko.length;
     }
-    if (q.indexOf(wko) === 0 && wko.length > bestWordLen) {
+    if (trimmed.indexOf(wko) === 0 && wko.length > bestWordLen) {
       bestWord = WORD_MAP[wko];
       bestWordLen = wko.length;
     }
@@ -328,7 +260,7 @@ function searchQuery(q) {
     return bestWord;
   }
 
-  return result.trim();
+  return trimmed;
 }
 
-export { SET_NUM_MAP };
+export { translateSearchQuery, SET_NUM_MAP };
