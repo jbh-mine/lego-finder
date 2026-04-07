d276525c8363933bf0d904fe4529a436cd80b556
jbh-mine <sinhwapen@gmail.com>
2026-04-07 17:17:09 +0900

    feat(theme): wrap App with ThemeProvider and import theme-dark.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './contexts/LanguageContext';
+import { ThemeProvider } from './contexts/ThemeContext';
 import Header from './components/Header';
 import SearchPage from './pages/SearchPage';
 import BrowsePage from './pages/BrowsePage';
@@ -14,29 +15,32 @@ import MocsPage from './pages/MocsPage';
 import MocDetailPage from './pages/MocDetailPage';
 import ScarcityPage from './pages/ScarcityPage';
 import './styles/App.css';
+import './styles/theme-dark.css';
 
 function App() {
   return (
-    <LanguageProvider>
-      <Router>
-        <Header />
-        <main className="main-container">
-          <Routes>
-            <Route path="/" element={<SearchPage />} />
-            <Route path="/browse" element={<BrowsePage />} />
-            <Route path="/parts" element={<PartsSearchPage />} />
-            <Route path="/part/:partNum" element={<PartDetailPage />} />
-            <Route path="/set/:setNum" element={<SetDetailPage />} />
-            <Route path="/collection" element={<CollectionPage />} />
-            <Route path="/new" element={<NewProductsPage />} />
-            <Route path="/funding" element={<FundingPage />} />
-            <Route path="/mocs" element={<MocsPage />} />
-            <Route path="/moc/:mocNum" element={<MocDetailPage />} />
-            <Route path="/scarcity" element={<ScarcityPage />} />
-          </Routes>
-        </main>
-      </Router>
-    </LanguageProvider>
+    <ThemeProvider>
+      <LanguageProvider>
+        <Router>
+          <Header />
+          <main className="main-container">
+            <Routes>
+              <Route path="/" element={<SearchPage />} />
+              <Route path="/browse" element={<BrowsePage />} />
+              <Route path="/parts" element={<PartsSearchPage />} />
+              <Route path="/part/:partNum" element={<PartDetailPage />} />
+              <Route path="/set/:setNum" element={<SetDetailPage />} />
+              <Route path="/collection" element={<CollectionPage />} />
+              <Route path="/new" element={<NewProductsPage />} />
+              <Route path="/funding" element={<FundingPage />} />
+              <Route path="/mocs" element={<MocsPage />} />
+              <Route path="/moc/:mocNum" element={<MocDetailPage />} />
+              <Route path="/scarcity" element={<ScarcityPage />} />
+            </Routes>
+          </main>
+        </Router>
+      </LanguageProvider>
+    </ThemeProvider>
   );
 }
 
